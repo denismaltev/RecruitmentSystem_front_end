63dac5fef43d9c010c719b452257f5c0686e04ec
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-04-15 16:49:29 -0700

    added react-bootstrap; got rid of reactstrap

--- recruitmentapp/src/App.js
+++ recruitmentapp/src/App.js
@@ -1,42 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import LogIn from './pages/LogIn'
 import Home from './pages/Home'
+
 
 export default class App extends React.Component{
   
   render() {
     return (
       <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    */}
-      <Router hashType='hashbang'>
-      <div>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/login' component={LogIn} />
-        </Switch>
+        <Router hashType='hashbang'>
+        <div>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/login' component={LogIn} />
+          </Switch>
+        </div>
+        </Router>
       </div>
-    </Router>
-    </div>
-    
     )
-
   }
 }
 
--- recruitmentapp/src/pages/LogIn.jsx
+++ recruitmentapp/src/pages/LogIn.jsx
@@ -1,36 +1,32 @@
 import React from 'react'
-import { Label } from 'reactstrap'
 import { Redirect, Link } from 'react-router-dom'
-
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 export default class LogIn extends React.Component {
     
     render(){
-      return(
+      return (
         <div>
-            <h1> Signin </h1>
+          <h1> Sign in </h1>
+          <br />
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
             <br/>
-            <Label htmlFor='email'> Email : 
-            <input
-              id='email'
-              type='email'
-              placeholder='Enter Your email'
-            />
-            </Label>
-            <br />
-            <Label htmlFor='password'> Password : 
-              <input
-                id='password'
-                type='password'
-                placeholder='Enter Your password'
-              />
-            </Label>
             <br/>
-            <Label>
-              <button> Signin </button>
-            </Label>
-            <br />
-          <Link to='/'>Problems logging in?</Link>
+            <p>Don't have an account?</p>
+            <Link to="/">Create Account</Link>
+          </Form>
         </div>
-      )
+      );
     }
 }
