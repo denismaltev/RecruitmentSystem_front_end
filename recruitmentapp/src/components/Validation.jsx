3ec976eeb213ff6af0485836c788c9a409cf2e61
Sara <sbanaeirad@my.bcit.ca>
2020-04-15 17:55:22 -0700

    Register(before connection to api)

---
+++ recruitmentapp/src/components/Validation.jsx
@@ -0,0 +1,36 @@
+export default function validateForm(event, state) {
+  // clear all error messages
+  const inputs = document.getElementsByClassName("is-danger");
+  for (let i = 0; i < inputs.length; i++) {
+    if (!inputs[i].classList.contains("error")) {
+      inputs[i].classList.remove("is-danger");
+    }
+  }
+
+  if (state.hasOwnProperty("username") && state.username === "") {
+    document.getElementById("username").classList.add("is-danger");
+    return { blankfield: true };
+  }
+  if (state.hasOwnProperty("email") && state.email === "") {
+    document.getElementById("email").classList.add("is-danger");
+    return { blankfield: true };
+  }
+  if (state.hasOwnProperty("password") && state.password === "") {
+    document.getElementById("password").classList.add("is-danger");
+    return { blankfield: true };
+  }
+  if (state.hasOwnProperty("confirmPassword") && state.confirmpassword === "") {
+    document.getElementById("confirmpassword").classList.add("is-danger");
+    return { blankfield: true };
+  }
+  if (
+    state.hasOwnProperty("password") &&
+    state.hasOwnProperty("confirmpassword") &&
+    state.password !== state.confirmpassword
+  ) {
+    document.getElementById("password").classList.add("is-danger");
+    document.getElementById("confirmpassword").classList.add("is-danger");
+    return { matchedpassword: true, blankfield: false };
+  }
+  return;
+}
