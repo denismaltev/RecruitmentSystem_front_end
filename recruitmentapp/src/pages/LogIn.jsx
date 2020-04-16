8c758037afd929bff12c095dc78b01aaae11108d
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-04-15 17:14:45 -0700

    simple login bootstrap styling

--- recruitmentapp/src/pages/LogIn.jsx
+++ recruitmentapp/src/pages/LogIn.jsx
@@ -1,32 +1,47 @@
 import React from 'react'
 import { Redirect, Link } from 'react-router-dom'
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import { Container, Row, Col } from "react-bootstrap";
 export default class LogIn extends React.Component {
     
     render(){
       return (
-        <div>
-          <h1> Sign in </h1>
-          <br />
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted"></Form.Text>
-            </Form.Group>
+        <Container>
+          <Row>
+            <Col className="p-5">
+              <form
+                style={{ margin: "0 auto", width: "500px" }}
+                className="text-center border border-light p-5"
+                action="#!"
+              >
+                <p className="h4 mb-4">Sign in</p>
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Login
-            </Button>
-            <br/>
-            <br/>
-            <p>Don't have an account?</p>
-            <Link to="/">Create Account</Link>
-          </Form>
-        </div>
+                <input
+                  type="email"
+                  className="form-control mb-4"
+                  placeholder="E-mail"
+                />
+
+                <input
+                  type="password"
+                  className="form-control mb-4"
+                  placeholder="Password"
+                />
+                <button
+                  onClick=""
+                  className="btn btn-primary btn-block my-4"
+                  type="submit"
+                >
+                  Login
+                </button>
+
+                <p>
+                  Not yet registered?
+                  <a href="/register"> Create an account</a>
+                </p>
+              </form>
+            </Col>
+          </Row>
+        </Container>
       );
     }
 }
