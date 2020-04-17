85c4d251e42f3f72d7a9012be66962a0cd8c40a2
Den79 <test08081979@gmail.com>
2020-04-17 14:10:01 -0700

    Login & token bug fixed

--- recruitmentapp/src/pages/LogIn.jsx
+++ recruitmentapp/src/pages/LogIn.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import FormErrors from "../components/FormError";
 import Validation from "../components/Validation";
@@ -8,30 +8,30 @@ const USER_ROLE = "";
 export default class LogIn extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       email: "",
       password: "",
       errors: {
-        blankfield: false,
-      },
+        blankfield: false
+      }
     };
-    this.login         = this.login.bind(this);
+    this.login = this.login.bind(this);
   }
 
   // Called when constructor is finished building component.
-  componentDidMount() {  
-    if(sessionStorage.getItem(AUTH_TOKEN)!=null) {
-      this.setState({ 
-        token:sessionStorage.getItem(AUTH_TOKEN)});
+  componentDidMount() {
+    if (sessionStorage.getItem(AUTH_TOKEN) != null) {
+      this.setState({
+        token: sessionStorage.getItem(AUTH_TOKEN)
+      });
     }
   }
-
 
   clearErrors = () => {
     this.setState({
       errors: {
-        blankfield: false,
+        blankfield: false
       }
     });
   };
@@ -43,117 +43,113 @@ export default class LogIn extends React.Component {
     document.getElementById(event.target.id).classList.remove("is-danger");
   };
 
-  login(e)  {
+  login(e) {
     // console.log(this.state.email);
-    const email      = this.email.value;
-    const password   = this.password.value;
-  
+    const email = this.email.value;
+    const password = this.password.value;
+
     //Prevent page reload
     e.preventDefault();
-  
+
     //Form validation
     this.clearErrors();
 
     const error = Validation(e, this.state);
     if (error) {
       this.setState({
-        errors: { ...this.state.errors, ...error },
+        errors: { ...this.state.errors, ...error }
       });
-    }else{
+    } else {
       const URL =
-      "https://recruitmentsystemapi.azurewebsites.net/api/auth/login";
+        "https://recruitmentsystemapi.azurewebsites.net/api/auth/login";
       fetch(URL, {
-        method: 'POST',
+        method: "POST",
         headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
+          Accept: "application/json",
+          "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            Email:      email, 
-            Password:   password
+          Email: email,
+          Password: password
         })
-    })
-    // Response received.
-    .then(response => response.json())
+      })
+        // Response received.
+        .then(response => response.json())
         // Data retrieved.
         .then(json => {
-            if(json["status"]==="OK") {
-  
-              this.props.auth.setUserRole( json["role"]) ;
-              this.props.auth.authenticateUser(true);
-              this.props.auth.setToken(this.token);
+          if (json["status"] === "OK") {
+            this.props.auth.setUserRole(json["role"]);
+            this.props.auth.authenticateUser(true);
+            this.props.auth.setToken(json["token"]);
 
-              this.props.history.push("./");
-            }
-            else {
-              this.setState({loginMessage:
-                "An error occured at login. Try again." }); 
-            }
+            this.props.history.push("./");
+          } else {
+            this.setState({
+              loginMessage: "An error occured at login. Try again."
+            });
+          }
         })
         // Data not retrieved.
-        .catch(function (error) {
-            if(sessionStorage[""])
-            alert(error);
-        }) 
-
+        .catch(function(error) {
+          if (sessionStorage[""]) alert(error);
+        });
     }
-  };
+  }
 
-    
-    render(){
-      return (
-        <Container>
-          <Row>
-            <Col className="p-5">
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col className="p-5">
             <FormErrors formerrors={this.state.errors} />
-              <form
-                style={{ margin: "0 auto", width: "500px" }}
-                className="text-center border border-light p-5"
-                // action="#!"
-                onSubmit={this.handleLogin}
+            <form
+              style={{ margin: "0 auto", width: "500px" }}
+              className="text-center border border-light p-5"
+              // action="#!"
+              onSubmit={this.handleLogin}
+            >
+              <p className="h1 mb-4">Sign in</p>
+
+              <input
+                type="email"
+                id="email"
+                className="form-control mb-4"
+                placeholder="E-mail"
+                value={this.state.email}
+                onChange={this.onInputChange}
+                ref={emailInput => (this.email = emailInput)}
+              />
+
+              <input
+                type="password"
+                id="password"
+                className="form-control mb-4"
+                placeholder="Password"
+                value={this.state.password}
+                onChange={this.onInputChange}
+                ref={passwordInput => (this.password = passwordInput)}
+              />
+              <button
+                onClick={this.login}
+                className="btn btn-primary btn-block my-4"
+                type="submit"
               >
-                <p className="h1 mb-4">Sign in</p>
+                Login
+              </button>
+              {/* <h1>{this.state.loginMessage}</h1> */}
+              {/* <br/>{this.state.token}<br/><br/> */}
+              <p className="control">
+                <a href="/">Forgot password?</a>
+              </p>
 
-                <input
-                  type="email"
-                  id="email"                  
-                  className="form-control mb-4"
-                  placeholder="E-mail"
-                  value={this.state.email}
-                  onChange={this.onInputChange}
-                  ref={(emailInput)=> this.email = emailInput}
-                />
-
-                <input
-                  type="password"
-                  id="password"
-                  className="form-control mb-4"
-                  placeholder="Password"
-                  value={this.state.password}
-                  onChange={this.onInputChange}
-                  ref={(passwordInput)=> this.password = passwordInput}
-                />
-                <button
-                  onClick={this.login}
-                  className="btn btn-primary btn-block my-4"
-                  type="submit"
-                >
-                  Login
-                </button>
-                {/* <h1>{this.state.loginMessage}</h1> */}
-                {/* <br/>{this.state.token}<br/><br/> */}
-                <p className="control">
-                    <a href="/">Forgot password?</a>
-                </p>
-
-                <p>
-                  Not yet registered?
-                  <a href="/#/registration"> Create an account</a>
-                </p>
-              </form>
-            </Col>
-          </Row>
-        </Container>
-      );
-    }
+              <p>
+                Not yet registered?
+                <a href="/#/registration"> Create an account</a>
+              </p>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
--- recruitmentapp/src/pages/RecruiterSkills.jsx
+++ recruitmentapp/src/pages/RecruiterSkills.jsx
@@ -1,32 +1,40 @@
 import React from "react";
-import { Table } from "react-bootstrap";
+import { Table, Button, InputGroup, FormControl } from "react-bootstrap";
 
 export default class RecruiterSkills extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      skills: [],
+      skillName: "",
+      chargeAmount: "",
+      payAmount: "",
+      skills: []
     };
   }
 
   getSkillsFromAPI = async () => {
-    var TOKEN =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbkBhZG1pbi5jb20iLCJqdGkiOiIwMThhY2VhNS02YjNkLTQ0N2YtYmU4MS03Y2Q2OGI3YjBlODQiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjA2ZjA1NDA2LWU4ODUtNDc4ZC1iYmFjLTZjNTgyZmFmY2YwYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFkbWluIiwiZXhwIjoxNTg3MTExOTE3LCJpc3MiOiJSZWNydWl0bWVudFN5c3RlbUFQSS5jYSIsImF1ZCI6IlJlY3J1aXRtZW50U3lzdGVtQVBJLmNhIn0.NiJDF1no4PPU0oueBHBTc_RzqVe_K1Cq3r1oNsc3u6I";
     var API_URL = "https://recruitmentsystemapi.azurewebsites.net/api/skills";
     await fetch(API_URL, {
       method: "GET",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
-        Authorization: `Bearer ${TOKEN}`,
-      },
+        Authorization: `Bearer ${this.props.auth.JWToken}`
+      }
     })
-      .then((res) => res.json())
-      .then((data) => {
+      .then(res => res.json())
+      .then(data => {
         this.setState({ skills: data });
         //console.log(data);
       });
     //console.log(this.skills);
+  };
+
+  onInputChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+    document.getElementById(event.target.id).classList.remove("is-danger");
   };
 
   async componentDidMount() {
@@ -37,6 +45,26 @@ export default class RecruiterSkills extends React.Component {
     return (
       <div>
         <h1> Recruiter Skills</h1>
+        <InputGroup className="mb-3">
+          <FormControl
+            onChange={this.onInputChange}
+            value={this.state.skillName}
+            placeholder="Skill"
+            aria-label="Skill"
+            aria-describedby="basic-addon1"
+          />
+          <FormControl
+            placeholder="Charge Amount"
+            aria-label="Charge Amount"
+            aria-describedby="basic-addon1"
+          />
+          <FormControl
+            placeholder="Pay Amount"
+            aria-label="Pay Amount"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+        <Button>Add Skill</Button>
         <Table striped bordered hover>
           <thead>
             <tr>
@@ -48,7 +76,7 @@ export default class RecruiterSkills extends React.Component {
           </thead>
           <tbody>
             {typeof this.state.skills !== "undefined" &&
-              this.state.skills.map((skill) => (
+              this.state.skills.map(skill => (
                 <tr key={skill.id}>
                   <td>{skill.name}</td>
                   <td> {skill.chargeAmount}</td>
