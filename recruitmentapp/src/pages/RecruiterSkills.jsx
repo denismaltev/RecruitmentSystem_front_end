a45ba39f55d4f7473afae3362acc978652508374
Den79 <test08081979@gmail.com>
2020-04-17 16:20:14 -0700

    code refactoring

--- recruitmentapp/src/pages/RecruiterSkills.jsx
+++ recruitmentapp/src/pages/RecruiterSkills.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Table, Button, InputGroup, FormControl } from "react-bootstrap";
 
+const TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbkBhZG1pbi5jb20iLCJqdGkiOiJiY2NkYWEzZi05NTIwLTRjYjEtYTM4Zi02MTRkZGEwY2IxMTQiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjA2ZjA1NDA2LWU4ODUtNDc4ZC1iYmFjLTZjNTgyZmFmY2YwYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFkbWluIiwiZXhwIjoxNTg3MTY1OTg2LCJpc3MiOiJSZWNydWl0bWVudFN5c3RlbUFQSS5jYSIsImF1ZCI6IlJlY3J1aXRtZW50U3lzdGVtQVBJLmNhIn0.WBHkbWumcekr5_vkdGhR2_kDmowcybXnvcfAza72xgY";
+const API_URL = "https://recruitmentsystemapi.azurewebsites.net/api/skills";
+
 export default class RecruiterSkills extends React.Component {
   constructor(props) {
     super(props);
@@ -10,10 +14,7 @@ export default class RecruiterSkills extends React.Component {
   }
 
   getSkillsFromAPI = async () => {
-    const TOKEN =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbkBhZG1pbi5jb20iLCJqdGkiOiJiY2NkYWEzZi05NTIwLTRjYjEtYTM4Zi02MTRkZGEwY2IxMTQiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjA2ZjA1NDA2LWU4ODUtNDc4ZC1iYmFjLTZjNTgyZmFmY2YwYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFkbWluIiwiZXhwIjoxNTg3MTY1OTg2LCJpc3MiOiJSZWNydWl0bWVudFN5c3RlbUFQSS5jYSIsImF1ZCI6IlJlY3J1aXRtZW50U3lzdGVtQVBJLmNhIn0.WBHkbWumcekr5_vkdGhR2_kDmowcybXnvcfAza72xgY";
     //const TOKEN = this.props.auth.JWToken;
-    var API_URL = "https://recruitmentsystemapi.azurewebsites.net/api/skills";
     await fetch(API_URL, {
       method: "GET",
       headers: {
@@ -34,10 +35,7 @@ export default class RecruiterSkills extends React.Component {
     var skillName = document.getElementById("skillName").value;
     var chargeAmount = document.getElementById("chargeAmount").value;
     var payAmount = document.getElementById("payAmount").value;
-    const TOKEN =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbkBhZG1pbi5jb20iLCJqdGkiOiJiY2NkYWEzZi05NTIwLTRjYjEtYTM4Zi02MTRkZGEwY2IxMTQiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjA2ZjA1NDA2LWU4ODUtNDc4ZC1iYmFjLTZjNTgyZmFmY2YwYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFkbWluIiwiZXhwIjoxNTg3MTY1OTg2LCJpc3MiOiJSZWNydWl0bWVudFN5c3RlbUFQSS5jYSIsImF1ZCI6IlJlY3J1aXRtZW50U3lzdGVtQVBJLmNhIn0.WBHkbWumcekr5_vkdGhR2_kDmowcybXnvcfAza72xgY";
     //const TOKEN = this.props.auth.JWToken;
-    var API_URL = "https://recruitmentsystemapi.azurewebsites.net/api/skills";
     await fetch(API_URL, {
       method: "POST",
       headers: {
