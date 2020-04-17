c54bd703b88e7de8666729dc013edcd2033900e9
thaqueubc <reya.ruet@gmail.com>
2020-04-16 17:14:51 -0700

    Connection to web API is added

--- recruitmentapp/src/pages/LogIn.jsx
+++ recruitmentapp/src/pages/LogIn.jsx
@@ -3,6 +3,10 @@ import { Container, Row, Col } from "react-bootstrap";
 import FormErrors from "../components/FormError";
 import Validation from "../components/Validation";
 
+// const BASE_URL        = 'https://localhost:44362/api/';
+
+const AUTH_TOKEN = "auth_token";
+
 export default class LogIn extends React.Component {
 
   constructor(props) {
@@ -10,12 +14,23 @@ export default class LogIn extends React.Component {
     this.state = {
       email: "",
       password: "",
-     
+      loginMessage : "",
       errors: {
         blankfield: false,
       },
     };
+    this.login         = this.login.bind(this);
+    // this.logout        = this.logout.bind(this);
   }
+
+  // Called when constructor is finished building component.
+  componentDidMount() {  
+    if(sessionStorage.getItem(AUTH_TOKEN)!=null) {
+      this.setState({ 
+        token:sessionStorage.getItem(AUTH_TOKEN)});
+    }
+  }
+
 
   clearErrors = () => {
     this.setState({
@@ -32,22 +47,61 @@ export default class LogIn extends React.Component {
     document.getElementById(event.target.id).classList.remove("is-danger");
   };
 
-  handleLogin= async event => {
-    console.log(this.state.email);
-
+  login(e)  {
+    // console.log(this.state.email);
+    const email      = this.email.value;
+    const password   = this.password.value;
+    // const URL           = BASE_URL + 'login';
     //Prevent page reload
-    event.preventDefault();
+    e.preventDefault();
   
     //Form validation
     this.clearErrors();
 
-    const error = Validation(event, this.state);
+    const error = Validation(e, this.state);
     if (error) {
       this.setState({
         errors: { ...this.state.errors, ...error },
       });
+    }else{
+
+      fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            Email:      email, 
+            Password:   password
+        })
+    })
+    // Response received.
+    .then(response => response.json())
+        // Data retrieved.
+        .then(json => {
+            // Store token with session data.
+            if(json["status"]==="OK") {
+              sessionStorage.setItem(AUTH_TOKEN, json["token"]);
+              this.token   = json["token"];
+              console.log(this.token);
+              this.setState({loginMessage:"The user has been logged in.",
+              token: json["token"] }); 
+            }
+            else {
+              this.setState({loginMessage:
+                "An error occured at login. Try again." }); 
+            }
+        })
+        // Data not retrieved.
+        .catch(function (error) {
+            if(sessionStorage[""])
+            alert(error);
+        }) 
+
     }
   };
+
     
     render(){
       return (
@@ -68,8 +122,9 @@ export default class LogIn extends React.Component {
                   id="email"                  
                   className="form-control mb-4"
                   placeholder="E-mail"
-                  value={this.state.email}
-                  onChange={this.onInputChange}
+                  // value={this.state.email}
+                  // onChange={this.onInputChange}
+                  ref={(emailInput)=> this.email = emailInput}
                 />
 
                 <input
@@ -77,11 +132,12 @@ export default class LogIn extends React.Component {
                   id="password"
                   className="form-control mb-4"
                   placeholder="Password"
-                  value={this.state.password}
-                  onChange={this.onInputChange}
+                  // value={this.state.password}
+                  // onChange={this.onInputChange}
+                  ref={(passwordInput)=> this.password = passwordInput}
                 />
                 <button
-                  // onClick=""
+                  onClick={this.login}
                   className="btn btn-primary btn-block my-4"
                   type="submit"
                 >
@@ -94,7 +150,7 @@ export default class LogIn extends React.Component {
 
                 <p>
                   Not yet registered?
-                  <a href="/register"> Create an account</a>
+                  <a href="/registration"> Create an account</a>
                 </p>
               </form>
             </Col>
