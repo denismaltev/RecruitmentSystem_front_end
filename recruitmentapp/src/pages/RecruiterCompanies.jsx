ac7e5ab57809d3128c104016d7a1bf5ee7349b39
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-04-17 19:37:22 -0700

    Render table data with map created; dummy data

--- recruitmentapp/src/pages/RecruiterCompanies.jsx
+++ recruitmentapp/src/pages/RecruiterCompanies.jsx
@@ -1,32 +1,72 @@
 import React from "react";
-const BASE_URL = "https://recruitmentsystemapi.azurewebsites.net/api/companies";
-const AUTH_TOKEN =
-  "auth_token";
+const URL = "https://recruitmentsystemapi.azurewebsites.net/api/companies";
+const numbers = [1, 2, 3, 4, 5];
 
 
 export default class RecruiterCompanies extends React.Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
       loggedIn: true,
-      companies: [],
-      item: {}
+      companies: [
+        {
+          id: "1",
+          name: "Company",
+          email: "email@email.com",
+          phone: "777-888-9999",
+          isActive: "yes",
+        },
+        {
+          id: "2",
+          name: "Company",
+          email: "email@email.com",
+          phone: "777-888-9999",
+          isActive: "yes",
+        },
+        {
+          id: "3",
+          name: "Company",
+          email: "email@email.com",
+          phone: "777-888-9999",
+          isActive: "yes",
+        },
+        {
+          id: "4",
+          name: "Company",
+          email: "email@email.com",
+          phone: "777-888-9999",
+          isActive: "yes",
+        }
+      ],
+      item: {},
+      token: "can't find",
     };
     this.getAll = this.getAll.bind(this);
+    this.getToken = this.getToken.bind(this);
   }
 
   componentDidMount(){
-    this.getAll();
+    //this.getAll();
+    this.getToken();
+  }
+
+  getToken(){
+    this.setState({token: this.props.auth.JWToken});
+    console.log(this.state.token)
   }
 
   getAll(){
-    const URL = BASE_URL;
+    this.setState({
+      token: this.props.JWToken,
+    });
+    console.log(this.state.token);
+    
     fetch(URL, {
       method: "GET",
       headers: {
-        Accept: "application/json",
+        "Accept": "application/json",
         "Content-Type": "application/json",
-        Authorization: `Bearer ${AUTH_TOKEN}`
+        "Authorization": `Bearer ${this.state.token}`
       }
     })
     .then(response => response.json())
@@ -38,29 +78,38 @@ export default class RecruiterCompanies extends React.Component {
       console.log(error);
     });
   }
+
+  renderTableData(){
+    return this.state.companies.map((company, index) => {
+      const {id, name, email, phone, isActive} = company
+      return (
+        <tr key={id}>
+          <th scope="row">{name}</th>
+          <td>{email}</td>
+          <td>{phone}</td>
+          <td>{isActive}</td>
+        </tr>
+      );
+    })
+  }
   
   render() {
     return (
       <div className="admin-companies">
         <h1> Recruiter Companies</h1>
-          <table className="table table-striped">
-            <thead className="table-secondary">
-              <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Phone</th>
-                <th scope="col">Active</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-              <th scope="row">Company</th>
-                <td>email@email.com</td>
-                <td>777-888-9999</td>
-                <td>Yes</td>
-              </tr>
-            </tbody>
-          </table>
+        <table className="table table-striped">
+          <thead className="table-secondary">
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
+              <th scope="col">Active</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderTableData()}
+          </tbody>
+        </table>
       </div>
     );
   }
