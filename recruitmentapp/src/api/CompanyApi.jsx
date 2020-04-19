5f6877554a54f6b57c941ad4e7699d8d4478fb58
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-04-19 13:57:42 -0700

    Added Company API file, added all company-related methods

---
+++ recruitmentapp/src/api/CompanyApi.jsx
@@ -0,0 +1,64 @@
+import { config } from "./config.json";
+import axios from "axios";
+
+export const getCompaniesList = ({ role }) => {
+    const options = {
+      url: config.BASE_API_URL + "companies/",
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+        Authorization: `Bearer ${this.props.auth.JWToken}`,
+      },
+      data: {},
+    };
+    return axios(options);
+};
+
+// update company (admin, company)
+export const updateCompany = ({ role, id }) => {
+    const options = {
+      url: config.BASE_API_URL + `companies/ ${id}`,
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+        Authorization: `Bearer ${this.props.auth.JWToken}`,
+      },
+      data: {
+        id: id,
+        name: name,
+        city: city,
+        province: province,
+        country: country,
+        address: address,
+        phone: phone,
+        isActive: isActive,
+      },
+    };
+     return axios(options);
+}
+
+// add company (company)
+export const updateCompany = ({ role }) => {
+  const options = {
+    url: config.BASE_API_URL + "companies/",
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json;charset=UTF-8",
+      Authorization: `Bearer ${this.props.auth.JWToken}`,
+    },
+    data: {
+      name: name,
+      city: city,
+      province: province,
+      country: country,
+      address: address,
+      phone: phone,
+      isActive: isActive,
+    },
+  };
+   return axios(options);
+};
+
