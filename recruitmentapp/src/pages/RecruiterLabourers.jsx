20dba48bad24a28a00a9d0f843ee3e829a71aa44
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-04-18 20:59:39 -0700

    Resolves #37

--- recruitmentapp/src/pages/RecruiterLabourers.jsx
+++ recruitmentapp/src/pages/RecruiterLabourers.jsx
@@ -56,6 +56,7 @@ export default class RecruiterLabourers extends React.Component {
               });
             }
 
+
             render() {
               return (
                 <div className="admin-labourers">
