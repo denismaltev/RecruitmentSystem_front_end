553abadcd8d7e9fc383bcedb5c5b19959105cacd
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-04-19 22:32:18 -0700

    Successfully edit info with info button

--- recruitmentapp/src/pages/RecruiterCompanies.jsx
+++ recruitmentapp/src/pages/RecruiterCompanies.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table } from "react-bootstrap";
+import { Table, Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import RecruiterCompany from "../components/RecruiterCompany";
 import { getCompaniesList } from "../api/CompaniesApi";
@@ -11,8 +11,6 @@ export default class RecruiterCompanies extends React.Component {
       companies: [],
     };
     this.getCompaniesListFromAPI = this.getCompaniesListFromAPI.bind(this);
-    this.updateCompanyToAPI = this.updateCompanyToAPI.bind(this);
-    this.startEditing = this.startEditing.bind(this);
   }
 
   componentDidMount() {
@@ -29,153 +27,153 @@ export default class RecruiterCompanies extends React.Component {
   };
 
   // this gets run on click of update button
-  updateCompanyToAPI = async (event) => {
-    const TOKEN = this.props.auth.JWToken;
-    const id = this.state.skill.id;
-    const companyName = this.state.companyName;
-    const city = this.state.city;
-    const province = this.state.province;
-    const country = this.state.country;
-    const address = this.state.address;
-    const phone = this.state.phone;
-    const email = this.state.email;
-    const isActive = this.state.skill.isActive;
-    await updateCompany({
-      TOKEN,
-      id,
-      companyName,
-      city,
-      province,
-      country,
-      address,
-      phone,
-      email,
-      isActive,
-    }).then((res) => {
-      if (res.status === 200) {
-        this.setState({ isEditable: false });
-      } else {
-        this.setState({ isEditable: false });
-        alert("Error: Something went wrong, please try again" + res.statusText);
-      }
-    });
-  };
+  // updateCompanyToAPI = async (event) => {
+  //   const TOKEN = this.props.auth.JWToken;
+  //   const id = this.state.skill.id;
+  //   const companyName = this.state.companyName;
+  //   const city = this.state.city;
+  //   const province = this.state.province;
+  //   const country = this.state.country;
+  //   const address = this.state.address;
+  //   const phone = this.state.phone;
+  //   const email = this.state.email;
+  //   const isActive = this.state.skill.isActive;
+  //   await updateCompany({
+  //     TOKEN,
+  //     id,
+  //     companyName,
+  //     city,
+  //     province,
+  //     country,
+  //     address,
+  //     phone,
+  //     email,
+  //     isActive,
+  //   }).then((res) => {
+  //     if (res.status === 200) {
+  //       this.setState({ isEditable: false });
+  //     } else {
+  //       this.setState({ isEditable: false });
+  //       alert("Error: Something went wrong, please try again" + res.statusText);
+  //     }
+  //   });
+  // };
 
-  startEditing() {
-    this.setState({ isEditable: true });
-  }
+  // startEditing() {
+  //   this.setState({ isEditable: true });
+  // }
 
-  onInputChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+  // onInputChange = (event) => {
+  //   this.setState({ [event.target.name]: event.target.value });
+  // };
 
-  renderEditableTable() {
-    return this.state.companies.map((company) => {
-      return (
-        <tr key={company.id}>
-          <th
-            scope="row"
-            id={company.id + "companyName"}
-            value={this.state.companyName}
-            name={"companyName"}
-            placeholder={company.name}
-            onChange={this.onInputChange}
-          ></th>
-          <td>
-            <input
-              id={company.id + "company-email"}
-              name={"email"}
-              value={this.state.email}
-              placeholder={company.email}
-              onChange={this.onInputChange}
-            ></input>
-          </td>
-          <td>
-            <input
-              id={company.id + "phone"}
-              value={this.state.phone}
-              name={"phone"}
-              placeholder={company.phone}
-              onChange={this.onInputChange}
-            ></input>
-          </td>
-          <td>
-            <input
-              id={company.address + "address"}
-              value={this.state.address}
-              name={"address"}
-              placeholder={company.address}
-              onChange={this.onInputChange}
-            ></input>
-          </td>
-          <td>
-            <input
-              id={company.city + "city"}
-              value={this.state.city}
-              name={"city"}
-              placeholder={company.city}
-              onChange={this.onInputChange}
-            ></input>
-          </td>
-          <td>
-            <input
-              id={company.province + "province"}
-              value={this.state.province}
-              name={"province"}
-              placeholder={company.province}
-              onChange={this.onInputChange}
-            ></input>
-          </td>
-          <td>
-            {company.isActive === true ? (
-              <FontAwesomeIcon icon="check-circle" color="blue" />
-            ) : (
-              ""
-            )}
-          </td>
-          <td>
-            <button
-              id="update-button"
-              className="btn btn-primary btn-sm"
-              onClick={this.updateCompanyToAPI}
-            >
-              Update
-            </button>
-          </td>
-        </tr>
-      );
-    });
-  }
+  // renderEditableTable() {
+  //   return this.state.companies.map((company) => {
+  //     return (
+  //       <tr key={company.id}>
+  //         <th
+  //           scope="row"
+  //           id={company.id + "companyName"}
+  //           value={this.state.companyName}
+  //           name={"companyName"}
+  //           placeholder={company.name}
+  //           onChange={this.onInputChange}
+  //         ></th>
+  //         <td>
+  //           <input
+  //             id={company.id + "company-email"}
+  //             name={"email"}
+  //             value={this.state.email}
+  //             placeholder={company.email}
+  //             onChange={this.onInputChange}
+  //           ></input>
+  //         </td>
+  //         <td>
+  //           <input
+  //             id={company.id + "phone"}
+  //             value={this.state.phone}
+  //             name={"phone"}
+  //             placeholder={company.phone}
+  //             onChange={this.onInputChange}
+  //           ></input>
+  //         </td>
+  //         <td>
+  //           <input
+  //             id={company.address + "address"}
+  //             value={this.state.address}
+  //             name={"address"}
+  //             placeholder={company.address}
+  //             onChange={this.onInputChange}
+  //           ></input>
+  //         </td>
+  //         <td>
+  //           <input
+  //             id={company.city + "city"}
+  //             value={this.state.city}
+  //             name={"city"}
+  //             placeholder={company.city}
+  //             onChange={this.onInputChange}
+  //           ></input>
+  //         </td>
+  //         <td>
+  //           <input
+  //             id={company.province + "province"}
+  //             value={this.state.province}
+  //             name={"province"}
+  //             placeholder={company.province}
+  //             onChange={this.onInputChange}
+  //           ></input>
+  //         </td>
+  //         <td>
+  //           {company.isActive === true ? (
+  //             <FontAwesomeIcon icon="check-circle" color="blue" />
+  //           ) : (
+  //             ""
+  //           )}
+  //         </td>
+  //         <td>
+  //           <button
+  //             id="update-button"
+  //             className="btn btn-primary btn-sm"
+  //             onClick={this.updateCompanyToAPI}
+  //           >
+  //             Update
+  //           </button>
+  //         </td>
+  //       </tr>
+  //     );
+  //   });
+  // }
 
-  renderTableData() {
-    return this.state.companies.map((company) => {
-      return (
-        <tr key={company.id}>
-          <th scope="row">{company.name}</th>
-          <td>{company.email}</td>
-          <td>{company.phone}</td>
-          <td>{company.address} </td>
-          <td>{company.city}</td>
-          <td>{company.province}</td>
-          <td>
-            {company.isActive === true ? (
-              <FontAwesomeIcon icon="check-circle" color="blue" />
-            ) : (
-              ""
-            )}
-          </td>
-          <td>
-            <button
-              className="btn btn-success btn-sm"
-              onClick={this.startEditing}
-            >
-              Edit
-            </button>
-          </td>
-        </tr>
-      );
-    });
-  }
+  // renderTableData() {
+  //   return this.state.companies.map((company) => {
+  //     return (
+  //       <tr key={company.id}>
+  //         <th scope="row">{company.name}</th>
+  //         <td>{company.email}</td>
+  //         <td>{company.phone}</td>
+  //         <td>{company.address} </td>
+  //         <td>{company.city}</td>
+  //         <td>{company.province}</td>
+  //         <td>
+  //           {company.isActive === true ? (
+  //             <FontAwesomeIcon icon="check-circle" color="blue" />
+  //           ) : (
+  //             ""
+  //           )}
+  //         </td>
+  //         <td>
+  //           <button
+  //             className="btn btn-success btn-sm"
+  //             onClick={this.startEditing}
+  //           >
+  //             Edit
+  //           </button>
+  //         </td>
+  //       </tr>
+  //     );
+  //   });
+  // }
 
   render() {
     return (
@@ -200,10 +198,12 @@ export default class RecruiterCompanies extends React.Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.isEditable === false
-              ? this.renderTableData()
-              : this.renderEditableTable()}
-            {/* {this.renderTableData()} */}
+            {typeof this.state.companies !== "undefined" &&
+              this.state.companies.map((company) => (
+                <tr key={company.id}>
+                  <RecruiterCompany {...this.props} company={company} />
+                </tr>
+              ))}
           </tbody>
         </Table>
       </div>
