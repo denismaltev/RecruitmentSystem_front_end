17220985d1c700fdfeca4572462c4a53516cb98f
Den79 <test08081979@gmail.com>
2020-04-20 02:04:34 -0700

    Add Skill: Validation and error messages

--- recruitmentapp/src/pages/RecruiterSkills.jsx
+++ recruitmentapp/src/pages/RecruiterSkills.jsx
@@ -10,12 +10,14 @@ export default class RecruiterSkills extends React.Component {
       skills: [],
       skillName: "",
       chargeAmount: "",
-      payAmount: ""
+      payAmount: "",
+      errorMessage: ""
     };
   }
 
   componentDidMount() {
     this.getSkillsFromAPI();
+    //console.log("a".isNaN);
   }
 
   onInputChange = event => {
@@ -40,32 +42,96 @@ export default class RecruiterSkills extends React.Component {
     this.setState({ skillName: "" });
     this.setState({ chargeAmount: "" });
     this.setState({ payAmount: "" });
+    this.setState({ errorMessage: "" });
+  };
+
+  isValid = (skillName, chargeAmount, payAmount) => {
+    // console.log(typeof skillName);
+    // console.log(typeof chargeAmount);
+    // console.log(typeof payAmount);
+    // console.log(skillName);
+    // console.log(chargeAmount);
+    // console.log(payAmount);
+
+    let errorMessage = "Error: ";
+    let result = true;
+
+    if (skillName.length === 0) {
+      errorMessage = errorMessage + "Skill field is empty. ";
+      result = false;
+    }
+    if (isNaN(chargeAmount)) {
+      errorMessage = errorMessage + "Charge Amount is not a number. ";
+      result = false;
+    }
+    if (isNaN(payAmount)) {
+      errorMessage = errorMessage + "Pay Amount is not a number. ";
+      result = false;
+    }
+    if (chargeAmount < 0) {
+      errorMessage = errorMessage + "Charge Amount is negative. ";
+      result = false;
+    }
+    if (payAmount < 0) {
+      errorMessage = errorMessage + "Pay Amount is negative. ";
+      result = false;
+    }
+    if (payAmount > chargeAmount) {
+      errorMessage =
+        errorMessage + "Pay Amount Must be less then charge amount ";
+      result = false;
+    }
+
+    if (result === false) {
+      this.setState({ errorMessage: errorMessage });
+    } else {
+      this.setState({ errorMessage: "" });
+    }
+    return result;
+    // if (
+    //   skillName !== "" &&
+    //   chargeAmount !== "" &&
+    //   payAmount !== "" &&
+    //   typeof skillName === "string" &&
+    //   typeof chargeAmount === "number" &&
+    //   typeof payAmount === "number" &&
+    //   chargeAmount > 0 &&
+    //   payAmount > 0
+    // ) {
+    //   return result;
+    // } else {
+    //   return result;
+    // }
   };
 
   addSkill = async event => {
     const TOKEN = this.props.auth.JWToken;
-    const skillName = this.state.skillName;
-    const chargeAmount = this.state.chargeAmount;
-    const payAmount = this.state.payAmount;
-    await postSkill({ TOKEN, skillName, chargeAmount, payAmount })
-      .then(res => {
-        if (res.status === 200) {
-          this.getSkillsFromAPI();
-          alert("New skill was added");
-          this.clearForm();
-        } else {
-          alert("ERROR: Something went wrong! " + res.statusText);
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        alert("ERROR: Something went wrong! ");
-      });
+    const skillName = this.state.skillName.trim();
+    const chargeAmount = parseFloat(this.state.chargeAmount);
+    const payAmount = parseFloat(this.state.payAmount);
+
+    if (this.isValid(skillName, chargeAmount, payAmount)) {
+      await postSkill({ TOKEN, skillName, chargeAmount, payAmount })
+        .then(res => {
+          if (res.status === 200) {
+            this.getSkillsFromAPI();
+            alert("New skill was added");
+            this.clearForm();
+          } else {
+            alert("ERROR: Something went wrong! " + res.statusText);
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          alert("ERROR: Something went wrong! ");
+        });
+    }
   };
 
   render() {
     return (
       <div>
+        <div style={{ color: "red" }}>{this.state.errorMessage}</div>
         <h1> Recruiter Skills</h1>
         <InputGroup className="mb-3">
           <FormControl
@@ -94,6 +160,14 @@ export default class RecruiterSkills extends React.Component {
             aria-describedby="basic-addon1"
           />
           <Button onClick={this.addSkill}>Add Skill</Button>
+          <Button
+            className="btn btn-danger btn-sm"
+            onClick={() => {
+              this.clearForm();
+            }}
+          >
+            X
+          </Button>
         </InputGroup>
         <Table striped bordered hover>
           <thead>
