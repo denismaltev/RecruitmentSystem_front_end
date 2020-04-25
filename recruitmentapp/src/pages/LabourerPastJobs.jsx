c07287589a351af6c7a75fcc7f1a9b28e2364124
Sara <sbanaeirad@my.bcit.ca>
2020-04-25 00:13:37 -0700

    #106 add post

--- recruitmentapp/src/pages/LabourerPastJobs.jsx
+++ recruitmentapp/src/pages/LabourerPastJobs.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Table } from "react-bootstrap";
 import StarRatings from "react-star-ratings";
 import { getAllLabourerjobs } from "../api/labourerJobApi";
+import { postRatings } from "../api/labourerJobApi";
 export default class LabourerPastJobs extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       jobList: [],
+      idToGrade: 0,
     };
     this.showJobList = this.showJobList.bind(this);
     this.changeRating = this.changeRating.bind(this);
@@ -16,11 +18,32 @@ export default class LabourerPastJobs extends React.Component {
   }
 
   changeRating(newRating, name) {
-    //Post
+    this.setState({
+      rating: newRating,
+    });
+  }
+
+  addRating() {
+    const token = this.props.auth.JWToken;
+    console.log(token);
+    const PARAM = `idToGrade=${this.state.idToGrade}&rating=${this.state.newRating}`;
+    postRatings({ token, PARAM })
+      .then((res) => {
+        if (res.status === 200) {
+          console.log(res.data);
+          this.setState({ jobResponse: res.data });
+          this.setState({ idToGrade: res.data.id });
+        } else {
+          alert("ERROR: Something went wrong! " + res.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert("Something went wrong! " + error.response.data.message);
+      });
   }
 
   async showJobList() {
-    const TOKEN = this.props.auth.JWToken;
+    const token = this.props.auth.JWToken;
     var count = 20;
     var today = new Date();
     var toDate = today.toISOString().split("T")[0];
@@ -30,23 +53,11 @@ export default class LabourerPastJobs extends React.Component {
     var page = 1;
     const PARAM = `count=${count}&toDate=${toDate}&page=${page}&fromDate=${fromDate}`;
     console.log(PARAM);
-    await getAllLabourerjobs({ TOKEN, PARAM })
+    await getAllLabourerjobs({ token, PARAM })
       .then((res) => {
         if (res.status === 200) {
           console.log(res.data);
           this.setState({ jobList: res.data });
-        } else {
-          alert("ERROR: Something went wrong! " + res.statusText);
-        }
-      })
-      .catch(function (error) {
-        alert("Something went wrong! " + error.response.data.message);
-      });
-    await getAllLabourerjobs({ TOKEN, PARAM })
-      .then((res) => {
-        if (res.status === 200) {
-          console.log(res.data);
-          this.setState({ jobResponse: res.data });
         } else {
           alert("ERROR: Something went wrong! " + res.statusText);
         }
@@ -62,24 +73,28 @@ export default class LabourerPastJobs extends React.Component {
         <tr key={item.id}>
           <td> {item.compnayName} </td>
           <td> {item.jobTitle} </td>
-          <td> {item.skillName} </td>
           <td> {item.address} </td>
-          <td> {item.date.toString().slice(0, 10)} </td>
+          <td> {item.date} </td>
           <td> {item.wageAmount} </td>
           <td>
+            {" "}
             <StarRatings
-              rating={item.jobRating ? item.jobRating : 0}
+              rating={this.state.rating}
               starRatedColor="blue"
               numberOfStars={5}
               name="rating"
               changeRating={this.changeRating}
-            />
+            />{" "}
             <h6>{this.state.rating}</h6>
+          </td>
+          <td>
+            <button onClick={this.addRating}>Rate the Job</button>
           </td>
         </tr>
       );
     });
   }
+
   render() {
     return (
       <div>
@@ -89,11 +104,11 @@ export default class LabourerPastJobs extends React.Component {
             <tr>
               <th>Company</th>
               <th>Job</th>
-              <th>Skill</th>
               <th>Address</th>
               <th>Date</th>
               <th>Wage</th>
               <th>Rating</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>{this.displayTableData()}</tbody>
