a2c6551451f1ce8e59c5f5d279af96c489049a71
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-04-28 17:35:24 -0700

    Minor tweak

--- recruitmentapp/src/pages/CompanyJobLabourers.jsx
+++ recruitmentapp/src/pages/CompanyJobLabourers.jsx
@@ -1,67 +1,72 @@
 import React, { useState, useEffect }from "react";
 import { Table } from "react-bootstrap";
+import Pagination from "../components/Pagination";
 import { getAllLabourerjobs } from "../api/labourerJobApi";
 
 
 export default class CompanyJobLabourers extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        isLoading: false,
-        job: {
-          id: props.jobId
-        },
-        labourers: [],
-      };
-    }
+                 constructor(props) {
+                   super(props);
+                   this.state = {
+                     isLoading: false,
+                     job: {
+                       id: props.jobId,
+                     },
+                     labourers: [],
+                   };
+                 }
 
-    componentDidMount() {
-      this.getLabourersListFromAPI();
-    }
+                 componentDidMount() {
+                   this.getLabourersListFromAPI();
+                 }
 
-    getLabourersListFromAPI = async () => {
-      const token    = this.props.auth.JWToken;
-      var count      = 10;
-      var pageNumber = this.state.page;
-      var jobId      = this.state.job.jobId;
-      var today      = new Date();
-      var fromDate   = today.toISOString().split("T")[0];
-      var toDate     = today.toISOString().split("T")[0];
+                 getLabourersListFromAPI = async () => {
+                   const token = this.props.auth.JWToken;
+                   var count = 10;
+                   var pageNumber = this.state.page;
+                   var jobId = this.state.job.jobId;
+                   var today = new Date();
+                   var fromDate = today.toISOString().split("T")[0];
+                   var toDate = today.toISOString().split("T")[0];
 
-      const param = `count=${count}&page=${pageNumber}&jobId=${jobId}&fromDate=${fromDate}&toDate=${toDate}`;
-      await getAllLabourerjobs({ token, param })
-      .then((res) => {
-        if(res.state === 200){
-          this.setState({ labourers: res.data.result });
-        } else {
-          
-        }
-        this.paginate = this.paginate.bind(this);
-      });
-    }
+                   const param = `count=${count}&page=${pageNumber}&jobId=${jobId}&fromDate=${fromDate}&toDate=${toDate}`;
+                   await getAllLabourerjobs({ token, param }).then((res) => {
+                     if (res.state === 200) {
+                       this.setState({ labourers: res.data.result });
+                     } else {
+                     }
+                     this.paginate = this.paginate.bind(this);
+                   });
+                 };
 
-    render() {
-      return (
-        <div className="page-content">
-          <h1>Job Title Placeholder Labourers List</h1>
-          <Table striped bordered hover>
-            <thead className="table-secondary">
-              {this.state.labourers.map}
-              <tr>
-                <th scope="col">Skill Name</th>
-                <th scope="col">Labourer Full Name</th>
-                <th scope="col">Labourer Phone Number</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Placeholder - Painter</td>
-                <td>Placeholder - John Smith</td>
-                <td>Placeholder - 777-888-9999</td>
-              </tr>
-            </tbody>
-          </Table>
-        </div>
-      );
-    }
-}
+                 paginate = (number) => {
+                   this.setState({ page: number }, () => {
+                     this.showJobList();
+                   });
+                 };
+
+                 render() {
+                   return (
+                     <div className="page-content">
+                       <h1>Job Title Placeholder Labourers List</h1>
+                       <Table striped bordered hover>
+                         <thead className="table-secondary">
+                           {this.state.labourers.map}
+                           <tr>
+                             <th scope="col">Skill Name</th>
+                             <th scope="col">Labourer Full Name</th>
+                             <th scope="col">Labourer Phone Number</th>
+                           </tr>
+                         </thead>
+                         <tbody>
+                           <tr>
+                             <td>Placeholder - Painter</td>
+                             <td>Placeholder - John Smith</td>
+                             <td>Placeholder - 777-888-9999</td>
+                           </tr>
+                         </tbody>
+                       </Table>
+                     </div>
+                   );
+                 }
+               }
