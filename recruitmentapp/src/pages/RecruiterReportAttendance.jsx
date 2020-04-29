05b3ffbc8bedc9b6cf5688bf9259958047afba10
Sara <sbanaeirad@my.bcit.ca>
2020-04-29 02:20:16 -0700

    #134 completed [not styled yet]

--- recruitmentapp/src/pages/RecruiterReportAttendance.jsx
+++ recruitmentapp/src/pages/RecruiterReportAttendance.jsx
@@ -2,59 +2,124 @@ import React from "react";
 import { Table } from "react-bootstrap";
 import LabourersSelector from "../components/LabourersSelector";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faTimes, faCheck } from "@fortawesome/free-solid-svg-icons";
+import { getAllLabourerjobs } from "../api/labourerJobApi";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class RecruiterReportAttendance extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      labourerId: 0,
+      idToSearch: 0,
       searchClicked: false,
       result: [],
+      errorMessage: "",
+      fromDate: new Date(),
+      toDate: new Date(),
     };
     this.search = this.search.bind(this);
   }
+
   selectLabourer = (selected) => {
-    console.log(selected);
-    this.setState({ labourerId: selected.id });
+    this.setState({ idToSearch: selected[0].id });
   };
 
-  search() {
-    this.setState({ searchClicked: true });
+  search = async (event) => {
+    if (this.state.idToSearch) {
+      const token = this.props.auth.JWToken;
+      var count = 20;
+      var pageNumber = 1;
+      var labourerId = this.state.idToSearch;
+      var start = this.state.fromDate.toISOString().split("T")[0];
+      var end = this.state.toDate.toISOString().split("T")[0];
+      const param = `count=${count}&page=${pageNumber}&fromDate=${start}&toDate=${end}&labourerId=${labourerId}`;
+      await getAllLabourerjobs({ token, param })
+        .then((res) => {
+          if (res.status === 200) {
+            this.setState({ result: res.data.result });
+          } else {
+            alert("ERROR: Something went wrong! " + res.statusText);
+          }
+        })
+        .catch(function (error) {
+          alert("Something went wrong! " + error.response.data.message);
+        });
+      this.setState({ errorMessage: "" });
+      this.setState({ searchClicked: true });
+    } else {
+      this.setState({ errorMessage: "Please choose a labourer" });
+    }
+  };
+
+  handleChange(date, flag) {
+    if (flag === 1) {
+      this.setState({
+        fromDate: date,
+      });
+    }
+    if (flag === 2) {
+      this.setState({
+        toDate: date,
+      });
+    }
   }
 
   displayTableData() {
     return this.state.result.map((item) => {
       return (
-        <tr>
-          <td>{item.date}</td>
+        <tr key={item.id}>
+          <td> {item.date.toString().slice(0, 10)}</td>
           <td>{item.companyName}</td>
           <td>{item.jobTitle}</td>
-          <td>{item.qualityRating}</td>
+          {item.qualityRating ? (
+            <td>
+              <FontAwesomeIcon icon={faCheck} color="green" />
+            </td>
+          ) : (
+            <td>
+              <FontAwesomeIcon icon={faTimes} color="red" />
+            </td>
+          )}
         </tr>
       );
     });
   }
   render() {
     return (
-      <div>
+      <div className="page-content">
         <div>
+          <DatePicker
+            name="fromDate"
+            selected={this.state.fromDate}
+            onSelect={this.handleSelect}
+            onChange={(date) => this.handleChange(date, 1)}
+          />
+          <DatePicker
+            name="toDate"
+            selected={this.state.toDate}
+            onSelect={this.handleSelect}
+            onChange={(date) => this.handleChange(date, 2)}
+          />
           <LabourersSelector
             auth={this.props.auth}
             selected={this.state.labourerId || 0}
             placeholder="Choose the labourer"
             onChange={this.selectLabourer}
           />
-          <button onClick={this.search}>
-            <FontAwesomeIcon icon={["fas", "fa-search"]} color="blue" />
+          <button className="search-button" onClick={this.search}>
+            <FontAwesomeIcon icon={faSearch} color="blue" />
           </button>
+          <h6>{this.state.errorMessage}</h6>
         </div>
+
         <div>
           {this.state.searchClicked && (
             <Table striped bordered hover>
               <thead className="table-secondary">
                 <tr>
-                  <th></th>
-                  <th>Company</th>
+                  <th>Date</th>
+                  <th>Company Name</th>
                   <th>Job Title</th>
                   <th>Attendance</th>
                 </tr>
