3fc6a7120bba53c9c63a1d25605553e5c46eed9d
AnnaBerman <aberman2@my.bcit.ca>
2020-05-01 16:57:28 -0700

    #195 sidebar mobile version + header with logout option

---
+++ recruitmentapp/src/components/Header.jsx
@@ -0,0 +1,129 @@
+import React, { Component } from "react";
+import {
+  Navbar,
+  Container,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import routes from "../routes";
+
+class Header extends Component {
+  state = {
+    isOpen: false,
+    color: "transparent",
+  };
+
+  sidebarToggle = React.createRef();
+
+  toggle = () => {
+    if (this.state.isOpen) {
+      this.setState({
+        color: "transparent",
+      });
+    } else {
+      this.setState({
+        color: "white",
+      });
+    }
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
+
+  openSidebar = () => {
+    document.documentElement.classList.toggle("nav-open");
+    this.sidebarToggle.current.classList.toggle("toggled");
+  };
+
+  updateColor = () => {
+    if (window.innerWidth < 993 && this.state.isOpen) {
+      this.setState({
+        color: "white",
+      });
+    } else {
+      this.setState({
+        color: "transparent",
+      });
+    }
+  };
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateColor.bind(this));
+  }
+
+  componentDidUpdate(e) {
+    if (
+      window.innerWidth < 993 &&
+      e.history.location.pathname !== e.location.pathname &&
+      document.documentElement.className.indexOf("nav-open") !== -1
+    ) {
+      document.documentElement.classList.toggle("nav-open");
+      this.sidebarToggle.current.classList.toggle("toggled");
+    }
+  }
+
+  render() {
+    return (
+      <Navbar
+        color={this.state.color}
+        expand="lg"
+        className={
+          "navbar-absolute fixed-top " +
+          (this.state.color === "transparent" ? "navbar-transparent " : "")
+        }
+      >
+        <Container fluid>
+          <div className="navbar-wrapper">
+            <div className="navbar-toggle">
+              <button
+                type="button"
+                ref={this.sidebarToggle}
+                className="navbar-toggler"
+                onClick={() => this.openSidebar()}
+              >
+                <span className="navbar-toggler-bar bar1" />
+                <span className="navbar-toggler-bar bar2" />
+                <span className="navbar-toggler-bar bar3" />
+              </button>
+            </div>
+          </div>
+          <NavbarToggler onClick={this.toggle}>
+            <span className="navbar-toggler-bar navbar-kebab" />
+            <span className="navbar-toggler-bar navbar-kebab" />
+            <span className="navbar-toggler-bar navbar-kebab" />
+          </NavbarToggler>
+          <Collapse
+            isOpen={this.state.isOpen}
+            navbar
+            className="justify-content-end"
+          >
+            <Nav navbar>
+              <NavItem>
+                <Link to="/logout" className="nav-link">
+                  <i className="now-ui-icons arrows-1_share-66" />
+                  <p>
+                    <span className="d-lg-none d-md-block">Logout</span>
+                  </p>
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link to="#" className="nav-link">
+                  <i className="now-ui-icons users_single-02" />
+                  <p>
+                    <span className="d-lg-none d-md-block">Account</span>
+                  </p>
+                </Link>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
+export default Header;
