ec85b08cb8b6d644e34af66d1006cb20cb7cbd01
Den79 <test08081979@gmail.com>
2020-05-01 00:51:23 -0700

    #182 styling

--- recruitmentapp/src/pages/RecruiterLabourerDetail.jsx
+++ recruitmentapp/src/pages/RecruiterLabourerDetail.jsx
@@ -16,59 +16,62 @@ const RecruiterLabourerDetail = props => {
   }, []);
   return (
     <div className="page-content">
-      {labourer.firstName}
-      <div class="card">
-        <div class="card-header">
-          Name: {labourer.firstName + " " + labourer.lastName}
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">Personal Id: {labourer.personalId}</li>
-          <li class="list-group-item">Email: {labourer.email}</li>
-          <li class="list-group-item">
-            Address:{" "}
-            {labourer.address +
-              " " +
-              labourer.city +
-              " " +
-              labourer.province +
-              " " +
-              labourer.country}
-          </li>
-          <li class="list-group-item">Phone: {labourer.phone}</li>
-          {/* <li class="list-group-item">
-            Skills:{" "}
-            {labourer.skills.map(s => (
-              <div>{s.name}</div>
-            ))}
-          </li> */}
-          <li class="list-group-item">
-            Safety Rating:{" "}
-            {
-              <StarRatings
-                rating={labourer.safetyRating}
-                starRatedColor="blue"
-                numberOfStars={5}
-                name="rating"
-                starDimension="30px"
-                starSpacing="1px"
-              />
-            }
-          </li>
-          <li class="list-group-item">
-            QualityRating:{" "}
-            {
-              <StarRatings
-                rating={labourer.qualityRating}
-                starRatedColor="blue"
-                numberOfStars={5}
-                name="rating"
-                starDimension="30px"
-                starSpacing="1px"
-              />
-            }
-          </li>
-        </ul>
+      <h2>{labourer.firstName + " " + labourer.lastName}</h2>
+      <div>
+        Skills:
+        {labourer.skills ? (
+          labourer.skills.map(s => (
+            <div key={s.id} className="badge badge-info">
+              {s.name}
+            </div>
+          ))
+        ) : (
+          <></>
+        )}
       </div>
+      <br />
+      <div>
+        Safety Rating:{" "}
+        {
+          <StarRatings
+            rating={labourer.safetyRating}
+            starRatedColor="blue"
+            numberOfStars={5}
+            name="rating"
+            starDimension="30px"
+            starSpacing="1px"
+          />
+        }
+      </div>
+      <div>
+        QualityRating:{" "}
+        {
+          <StarRatings
+            rating={labourer.qualityRating}
+            starRatedColor="blue"
+            numberOfStars={5}
+            name="rating"
+            starDimension="30px"
+            starSpacing="1px"
+          />
+        }
+      </div>
+      <br />
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">Personal Id: {labourer.personalId}</li>
+        <li className="list-group-item">Email: {labourer.email}</li>
+        <li className="list-group-item">
+          Address:{" "}
+          {labourer.address +
+            ". " +
+            labourer.city +
+            ". " +
+            labourer.province +
+            ". " +
+            labourer.country}
+        </li>
+        <li className="list-group-item">Phone: {labourer.phone}</li>
+      </ul>
     </div>
   );
 };
