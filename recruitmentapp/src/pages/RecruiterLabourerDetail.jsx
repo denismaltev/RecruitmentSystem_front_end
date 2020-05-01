7fad232f54f1d2b8e460d664bd144ccaaedf4dbe
Den79 <test08081979@gmail.com>
2020-04-30 22:09:51 -0700

    #181 Adress, Ratings removed

--- recruitmentapp/src/pages/RecruiterLabourerDetail.jsx
+++ recruitmentapp/src/pages/RecruiterLabourerDetail.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import StarRatings from "react-star-ratings";
 
 const RecruiterLabourerDetail = props => {
-  return <div>RecruiterLabourerDetail</div>;
+  return (
+    <div>
+      RecruiterLabourerDetail
+      {/* <td>
+            <StarRatings
+              rating={labourer.safetyRating}
+              starRatedColor="blue"
+              numberOfStars={5}
+              name="rating"
+              starDimension="30px"
+              starSpacing="1px"
+            />
+          </td>
+          <td>
+            <StarRatings
+              rating={labourer.qualityRating}
+              starRatedColor="blue"
+              numberOfStars={5}
+              name="rating"
+              starDimension="30px"
+              starSpacing="1px"
+            />
+          </td> */}
+    </div>
+  );
 };
 
 export default RecruiterLabourerDetail;
--- recruitmentapp/src/pages/RecruiterLabourers.jsx
+++ recruitmentapp/src/pages/RecruiterLabourers.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import { Table } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { getAllLabourers } from "../api/LabourerApi";
-import StarRatings from "react-star-ratings";
 import Pagination from "../components/Pagination";
 import { config } from "../api/config.json";
-import RecruiterLabourerDetail from "../pages/RecruiterLabourerDetail";
 
 export default class RecruiterLabourers extends React.Component {
   constructor(props) {
@@ -56,33 +54,12 @@ export default class RecruiterLabourers extends React.Component {
           </th>
           <td>{labourer.phone}</td>
           <td>{labourer.email}</td>
-          <td>{labourer.address}</td>
           <td>
             {labourer.isActive === true ? (
               <FontAwesomeIcon icon="check-circle" color="blue" />
             ) : (
               ""
             )}
-          </td>
-          <td>
-            <StarRatings
-              rating={labourer.safetyRating}
-              starRatedColor="blue"
-              numberOfStars={5}
-              name="rating"
-              starDimension="30px"
-              starSpacing="1px"
-            />
-          </td>
-          <td>
-            <StarRatings
-              rating={labourer.qualityRating}
-              starRatedColor="blue"
-              numberOfStars={5}
-              name="rating"
-              starDimension="30px"
-              starSpacing="1px"
-            />
           </td>
         </tr>
       );
@@ -109,10 +86,7 @@ export default class RecruiterLabourers extends React.Component {
               <th scope="col">Full Name</th>
               <th scope="col">Phone</th>
               <th scope="col">Email</th>
-              <th scope="col">Address</th>
               <th scope="col">Active</th>
-              <th scope="col">Safety Rating</th>
-              <th scope="col">Quality Rating</th>
             </tr>
           </thead>
           <tbody>{this.renderTableData()}</tbody>
