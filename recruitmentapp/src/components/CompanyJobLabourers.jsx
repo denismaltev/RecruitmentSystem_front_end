ceb9b1f373635952dcbf9a7999fba1e4dcf494d0
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-05-01 15:02:56 -0700

    Added React template cards for all company jobs. Issue #201

--- recruitmentapp/src/components/CompanyJobLabourers.jsx
+++ recruitmentapp/src/components/CompanyJobLabourers.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import { Table } from "react-bootstrap";
-import Pagination from "../components/Pagination";
+import { Row, Col, Card, CardBody } from "reactstrap";
 import { getLabourerJobs } from "../api/labourerJobApi";
+import { config } from "../api/config.json";
+import PanelHeader from "../components/PanelHeader";
+import Pagination from "../components/Pagination";
 
+var count = config.NUMBER_OF_ROWS_PER_PAGE;
 export default class CompanyJobLabourers extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      labourers: [],
+      labourerFullName: "",
+      labourerPhone: "",
+      skillName: "",
       page: 1,
       isLoading: true,
       jobId: props.match.params.id,
-      labourers: [],
     };
   }
 
   componentDidMount() {
     this.getLabourersListFromAPI();
-    console.log(this.state.jobId);
   }
 
   getLabourersListFromAPI = () => {
@@ -47,28 +53,37 @@ export default class CompanyJobLabourers extends React.Component {
     // if (this.state.isLoading) return <div>Loading...</div>;
     // else {
       return (
+        <>
+        <PanelHeader size="sm" />
         <div className="page-content">
-          <h1>Labourers List</h1>
-          <Table striped bordered hover>
-            <thead className="table-secondary">
-              <tr>
-                <th scope="col">Skill Name</th>
-                <th scope="col">Labourer Full Name</th>
-                <th scope="col">Labourer Phone Number</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.labourers.map((labourer, index) => (
-                <tr key={index}>
-                  <td>{labourer.skillName}</td>
-                  <td>{labourer.labourerFullName}</td>
-                  <td>{labourer.labourerPhone}</td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-          <Pagination paginate={this.paginate} />
-        </div>
+         <Row>
+            <Col xs={12}>
+              <Card>
+                <CardBody>
+                    <Table striped bordered hover>
+                        <thead className="table-secondary">
+                        <tr>
+                            <th scope="col">Skill Name</th>
+                            <th scope="col">Labourer Full Name</th>
+                            <th scope="col">Labourer Phone Number</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {this.state.labourers.map((labourer, index) => (
+                            <tr key={index}>
+                            <td>{labourer.skillName}</td>
+                            <td>{labourer.labourerFullName}</td>
+                            <td>{labourer.labourerPhone}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </Table>
+                </CardBody>
+              </Card>
+            </Col>
+        </Row>
+      </div>
+      </>
       );
     }
   }
--- recruitmentapp/src/pages/CompanyJobs.jsx
+++ recruitmentapp/src/pages/CompanyJobs.jsx
@@ -3,28 +3,32 @@ import { getCompanyJobs, putJob, getJobById } from "../api/JobsApi";
 import { Table } from "react-bootstrap";
 import Weekdays from "../components/Weekdays";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { config } from "../api/config.json";
+import PanelHeader from "../components/PanelHeader";
+import { Row, Col, Card, CardBody } from "reactstrap";
 
-const CompanyJobs = props => {
+var count = config.NUMBER_OF_ROWS_PER_PAGE;
+const CompanyJobs = (props) => {
   const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
-    getCompanyJobs({ token: props.auth.JWToken }).then(response => {
+    getCompanyJobs({ token: props.auth.JWToken }).then((response) => {
       setJobs(response.data.result);
     });
   }, [props.auth.JWToken]);
 
-  const handleAddJobClick = job => {
+  const handleAddJobClick = (job) => {
     props.history.push("./company-job-detail/" + (job ? job.id : "add"));
   };
 
-  const handleViewLabourers = job => {
+  const handleViewLabourers = (job) => {
     props.history.push("./company-job-labourers/" + job.id);
   };
 
-  const changeActiveStatus = currentJob => {
+  const changeActiveStatus = (currentJob) => {
     // for changing picture start
     setJobs(
-      jobs.map(item =>
+      jobs.map((item) =>
         item.id === currentJob.id
           ? { ...item, isActive: item.isActive ? false : true }
           : item
@@ -34,13 +38,13 @@ const CompanyJobs = props => {
 
     // for changing state in back-end start
     getJobById({ TOKEN: props.auth.JWToken, id: currentJob.id }).then(
-      response => {
+      (response) => {
         let job = response.data;
         job.isActive = job.isActive ? false : true;
         putJob({
           TOKEN: props.auth.JWToken,
           id: response.data.id,
-          job: job
+          job: job,
         });
       }
     );
@@ -48,72 +52,85 @@ const CompanyJobs = props => {
   };
 
   return (
-    <div className="page-content">
-      <button
-        type="button"
-        className="btn btn-primary btn-sm"
-        onClick={() => handleAddJobClick()}
-      >
-        Add Job
-      </button>
-      <Table striped bordered hover>
-        <thead className="table-secondary">
-          <tr>
-            <th scope="col">Title</th>
-            <th scope="col">Address</th>
-            <th scope="col">Start date</th>
-            <th scope="col">End date</th>
-            <th scope="col">Days of week</th>
-            <th scope="col">Active</th>
-            <th scope="col">Assigned Labourers</th>
-          </tr>
-        </thead>
-        <tbody>
-          {jobs.map((job, index) => (
-            <tr key={index}>
-              <td onClick={() => handleAddJobClick(job)}>{job.title}</td>
-              <td onClick={() => handleAddJobClick(job)}>
-                {job.address}, {job.city}
-              </td>
-              <td onClick={() => handleAddJobClick(job)}>
-                {new Date(job.startDate).toLocaleDateString()}
-              </td>
-              <td onClick={() => handleAddJobClick(job)}>
-                {new Date(job.endDate).toLocaleDateString()}
-              </td>
-              <td onClick={() => handleAddJobClick(job)}>
-                <Weekdays
-                  days={{
-                    mon: job.monday,
-                    tue: job.tuesday,
-                    wed: job.wednesday,
-                    thu: job.thursday,
-                    fri: job.friday,
-                    sat: job.saturday,
-                    sun: job.sunday
-                  }}
-                />
-              </td>
-              <td onClick={() => changeActiveStatus(job)}>
-                {job.isActive === true ? (
-                  <FontAwesomeIcon icon="check-circle" color="blue" />
-                ) : (
-                  <div>X</div>
-                )}
-              </td>
-              <td>
+    <>
+      <PanelHeader size="sm" />
+      <div className="page-content">
+        <Row>
+          <Col xs={12}>
+            <Card>
+              <CardBody>
                 <button
-                  className="btn btn-success"
-                  onClick={() => handleViewLabourers(job)}
+                  type="button"
+                  className="btn btn-primary btn-sm"
+                  onClick={() => handleAddJobClick()}
                 >
-                  View Labourers
+                  Add Job
                 </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </div>
+                <Table striped bordered hover>
+                  <thead className="table-secondary">
+                    <tr>
+                      <th scope="col">Title</th>
+                      <th scope="col">Address</th>
+                      <th scope="col">Start date</th>
+                      <th scope="col">End date</th>
+                      <th scope="col">Days of week</th>
+                      <th scope="col">Active</th>
+                      <th scope="col">Assigned Labourers</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {jobs.map((job, index) => (
+                      <tr key={index}>
+                        <td onClick={() => handleAddJobClick(job)}>
+                          {job.title}
+                        </td>
+                        <td onClick={() => handleAddJobClick(job)}>
+                          {job.address}, {job.city}
+                        </td>
+                        <td onClick={() => handleAddJobClick(job)}>
+                          {new Date(job.startDate).toLocaleDateString()}
+                        </td>
+                        <td onClick={() => handleAddJobClick(job)}>
+                          {new Date(job.endDate).toLocaleDateString()}
+                        </td>
+                        <td onClick={() => handleAddJobClick(job)}>
+                          <Weekdays
+                            days={{
+                              mon: job.monday,
+                              tue: job.tuesday,
+                              wed: job.wednesday,
+                              thu: job.thursday,
+                              fri: job.friday,
+                              sat: job.saturday,
+                              sun: job.sunday,
+                            }}
+                          />
+                        </td>
+                        <td onClick={() => changeActiveStatus(job)}>
+                          {job.isActive === true ? (
+                            <FontAwesomeIcon icon="check-circle" color="blue" />
+                          ) : (
+                            <div>X</div>
+                          )}
+                        </td>
+                        <td>
+                          <button
+                            className="btn btn-success"
+                            onClick={() => handleViewLabourers(job)}
+                          >
+                            View Labourers
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
   );
 };
 
