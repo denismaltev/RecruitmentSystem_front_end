f3ff2f6993095d1ccfafe604882461d7e564c945
thaqueubc <reya.ruet@gmail.com>
2020-05-03 12:15:46 -0700

    table is placed inside of a card

--- recruitmentapp/src/pages/SafetyReport.jsx
+++ recruitmentapp/src/pages/SafetyReport.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { getLabourerJobs } from "../api/labourerJobApi";
 import { Table } from "react-bootstrap";
+import PanelHeader from "../components/PanelHeader";
 import SafetyReportItem from "../components/SafetyReportItem";
 import StarRatings from "react-star-ratings";
+import { Row, Col, Card, CardBody,InputGroup, } from "reactstrap";
 
 const SafetyReport = (props) => {
   const [data, setData] = useState([]);
@@ -27,25 +29,36 @@ const SafetyReport = (props) => {
   }, [page, props.auth.JWToken]);
 
   return (
-    <div className="page-content">
-      <Table striped bordered hover>
-        <thead className="table-secondary">
-          <tr>
-            <th>Labourer full name</th>
-            <th>Labourer phone</th>
-            <th>Job title</th>
-            <th>Job skill</th>
-            <th>Date</th>
-            <th>Safety rating</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item, index) => (
-            <SafetyReportItem key={index} {...props} item={item} />
-          ))}
-        </tbody>
-      </Table>
+    <>
+    <PanelHeader size="sm" />
+    <div className="content">
+
+      <Row>
+         <Col>
+            <Card>
+            <Table responsive striped bordered hover>
+              <thead className="table-secondary">
+                <tr>
+                  <th>Labourer full name</th>
+                  <th>Labourer phone</th>
+                  <th>Job title</th>
+                  <th>Job skill</th>
+                  <th>Date</th>
+                  <th>Safety rating</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((item, index) => (
+                  <SafetyReportItem key={index} {...props} item={item} />
+                ))}
+              </tbody>
+            </Table>
+            </Card>
+         </Col>
+      </Row>
+     
     </div>
+    </>
   );
 };
 
