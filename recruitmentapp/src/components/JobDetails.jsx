6e14823c9ef3940d5d195dfe93eda63987c68c2d
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-05-03 00:51:48 -0700

    Added star ratings, title, fixed weekday buttons. Issue #201

--- recruitmentapp/src/components/JobDetails.jsx
+++ recruitmentapp/src/components/JobDetails.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Card, CardBody } from "reactstrap";
+import { Card, CardBody, CardTitle } from "reactstrap";
+import StarRatings from "react-star-ratings";
 import { Table } from "react-bootstrap";
 
 const MONTHS = [
@@ -28,61 +29,77 @@ export default function JobDetail(props) {
   return (
     <Card>
       <CardBody>
+              <CardTitle style={{ textAlign: "center" }}>
+          <h3 style={{ margin: 0 }}>
+            {props.selectedJob.title} Details
+          </h3>
+          <div >
+            <StarRatings
+              id="rating"
+              rating={props.selectedJob.rating}
+              starRatedColor="blue"
+              numberOfStars={5}
+              starDimension="30px"
+              name="rating"
+                          starSpacing="5px"
+            />
+          </div>
+        </CardTitle>
+
         <Table>
-            <tbody>
-              <tr>
-            <th>Address</th>
-            <td>{props.selectedJob.address}</td>
-          </tr>
-          <tr>
-            <th>Dates</th>
-            <td>
-              {formatDate(props.selectedJob.startDate)} -{" "}
-              {formatDate(props.selectedJob.endDate)}
-            </td>
-          </tr>
-          <tr>
-            <th>Weekdays</th>
-            <td>
-              {props.selectedJob.sunday && (
-                <button disabled className="weekday-tags">
-                  Sun
-                </button>
-              )}
-              {props.selectedJob.monday && (
-                <button disabled className="weekday-tags">
-                  Mon
-                </button>
-              )}
-              {props.selectedJob.tuesday && (
-                <button disabled className="weekday-tags">
-                  Tue
-                </button>
-              )}
-              {props.selectedJob.wednesday && (
-                <button disabled className="weekday-tags">
-                  Wed
-                </button>
-              )}
-              {props.selectedJob.thursday && (
-                <button disabled className="weekday-tags">
-                  Thu
-                </button>
-              )}
-              {props.selectedJob.friday && (
-                <button disabled className="weekday-tags">
-                  Fri
-                </button>
-              )}
-              {props.selectedJob.saturday && (
-                <button disabled className="weekday-tags">
-                  Sat
-                </button>
-              )}
-            </td>
-          </tr>  
-            </tbody>
-          
+          <tbody>
+            <tr>
+              <th>Address</th>
+              <td>{props.selectedJob.address}</td>
+            </tr>
+            <tr>
+              <th>Dates</th>
+              <td>
+                {formatDate(props.selectedJob.startDate)} -{" "}
+                {formatDate(props.selectedJob.endDate)}
+              </td>
+            </tr>
+            <tr>
+              <th>Weekdays</th>
+              <td>
+                {props.selectedJob.sunday && (
+                  <button disabled className="weekday-tags">
+                    Sun
+                  </button>
+                )}
+                {props.selectedJob.monday && (
+                  <button disabled className="weekday-tags">
+                    Mon
+                  </button>
+                )}
+                {props.selectedJob.tuesday && (
+                  <button disabled className="weekday-tags">
+                    Tue
+                  </button>
+                )}
+                {props.selectedJob.wednesday && (
+                  <button disabled className="weekday-tags">
+                    Wed
+                  </button>
+                )}
+                {props.selectedJob.thursday && (
+                  <button disabled className="weekday-tags">
+                    Thu
+                  </button>
+                )}
+                {props.selectedJob.friday && (
+                  <button disabled className="weekday-tags">
+                    Fri
+                  </button>
+                )}
+                {props.selectedJob.saturday && (
+                  <button disabled className="weekday-tags">
+                    Sat
+                  </button>
+                )}
+              </td>
+            </tr>
+          </tbody>
         </Table>
       </CardBody>
     </Card>
